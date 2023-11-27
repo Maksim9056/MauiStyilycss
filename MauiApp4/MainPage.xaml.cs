aaa889283e34c2e771da335078924ee05382e0ce
Maks <bobreczovm@inbox.ru>
2023-11-27 08:45:59 +0300

    Добавьте файлы проекта.

---
+++ MauiApp4/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Maui.Controls.StyleSheets;
+using System.Reflection;
+
+namespace MauiApp4
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+
+            InitializeComponent();
+        }
+            
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            //this.Resources.Remove("Resources/Styles/mystyles.css");
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.Add(StyleSheet.FromResource
+           ("Resources/Styles/mystyles.css", IntrospectionExtensions.GetTypeInfo(typeof(MainPage)).Assembly));
+        }
+        private void OnCounterClicked2(object sender, EventArgs e)
+        {
+            //this.Resources.Remove("Resources/Styles/mystyles.css");
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.Add(StyleSheet.FromResource
+           ("Resources/Styles/mystyles2.css", IntrospectionExtensions.GetTypeInfo(typeof(MainPage)).Assembly));
+        }
+        private void OnCounterClicked3(object sender, EventArgs e)
+        {
+            this.Resources.Clear();
+            Resources.MergedDictionaries.Clear();
+            Resources.Clear();
+            Application.Current.Resources.Clear();
+
+            // Очистите содержимое страницы
+        }
+
+
+    }
+
+}
